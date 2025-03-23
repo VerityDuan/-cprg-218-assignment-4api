bb89eb433a069f85d7981e7ef742aac3465aad16
VerityDuan <verityduan@gmail.com>
2025-03-22 22:16:05 -0600

    Fetches API using link provided

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     fetchJoke("Any"); // Allow page to load a random joke 
 });
 
+async function fetchJoke(category) { // fetches joke API
+    try {
+        const response = await fetch(`https://v2.jokeapi.dev/joke/${category}?safe-mode`);
+        const data = await response.json();}}
 
