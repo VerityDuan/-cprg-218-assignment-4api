0026c9523d58e774bc3571e031ec02903c10ff19
VerityDuan <verityduan@gmail.com>
2025-03-22 22:17:59 -0600

    single line versus two part lines

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetchJoke("Any"); // Allow page to load a random joke 
+    fetchJoke("Any"); // Load a random joke when the page loads
 });
 
-async function fetchJoke(category) { // fetches joke API
+async function fetchJoke(category) {
     try {
         const response = await fetch(`https://v2.jokeapi.dev/joke/${category}?safe-mode`);
-        const data = await response.json();}}
-
+        const data = await response.json();
+        
+        let jokeText = "";
+        if (data.type === "single") {
+            jokeText = data.joke;
+        } else if (data.type === "twopart") {
+            jokeText = `${data.setup} ... ${data.delivery}`;
+        }
+    }}
